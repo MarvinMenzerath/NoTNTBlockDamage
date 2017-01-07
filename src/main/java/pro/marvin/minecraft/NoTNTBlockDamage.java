4de120979fe7108c5263569320c970bbf1aa4dba
Marvin Menzerath <github@marvin-menzerath.de>
2017-01-07 20:31:12 +0100

    Updated dependencies and moved to Java 8

---
+++ src/main/java/pro/marvin/minecraft/NoTNTBlockDamage.java
@@ -0,0 +1,19 @@
+package pro.marvin.minecraft;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoTNTBlockDamage extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onEntityExplode(EntityExplodeEvent e) {
+		e.blockList().clear();
+	}
+}
